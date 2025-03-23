af92086fa8100beac52735c7b0e1fb85accde2d5
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2025-03-22 22:22:53 -0300

    cria o roteador da aplicação (wip)

---
+++ server/router.php
@@ -0,0 +1,23 @@
+<!-- Roteador da Aplicação -->
+
+<?php
+session_start();
+
+$page = $_GET["page"] ?? "index";
+
+$private_pages = ["carrinho", "vendas"];
+
+if (in_array($page, $private_pages) && !isset($_SESSION["user_id"])) {
+    header("Location: index.html");
+    exit;
+}
+
+$file = __DIR__ . "/../templates/" . $page . ".html";
+
+if (file_exists($file)) {
+    readfile($file);
+} else {
+    http_response_code(404);
+    echo "Página não encontrada!";
+}
+?>
