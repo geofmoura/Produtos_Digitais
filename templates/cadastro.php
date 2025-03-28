677c81b9ccf4ec59c03f5424a90efa8a31421a42
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2025-03-28 15:29:22 -0300

    Cadastra o usuário

--- templates/cadastro.php
+++ templates/cadastro.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <form id="formCadastro" method="POST">
     <input type="text" name="nome" placeholder="Nome" required>
     <input type="email" name="email" placeholder="E-mail" required>
     <input type="password" name="senha" placeholder="Senha" required>
-    <button type="submit">Cadastrar</button>
+    <a href="vendas.php"><button type="submit">Cadastrar</button></a>
 </form>
 <p id="mensagemCadastro"></p>
+<a href="../index.php"><button>Voltar</button></a>
+</body>
+</html>
+
+
 
