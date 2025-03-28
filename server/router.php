12ca12db3e13ce6ed8e1c33e78b0e0bc7c452d20
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2025-03-28 15:28:59 -0300

    Roteador da Aplicação

--- server/router.php
+++ server/router.php
@@ -1,23 +1,25 @@
-<!-- Roteador da Aplicação -->
-
 <?php
+ob_start(); // Inicia o buffer de saída para evitar erros de header
 session_start();
 
 $page = $_GET["page"] ?? "index";
 
 $private_pages = ["carrinho", "vendas"];
 
-if (in_array($page, $private_pages) && !isset($_SESSION["user_id"])) {
-    header("Location: index.php");
+
+if (isset($_SESSION["user_id"])) {
+    header("Location: ../server/router.php?page=vendas");
     exit;
 }
 
 $file = __DIR__ . "/../templates/" . $page . ".php";
 
 if (file_exists($file)) {
-    readfile($file);
+    include $file; 
 } else {
     http_response_code(404);
     echo "Página não encontrada!";
 }
+
+ob_end_flush(); // Libera o buffer de saída
 ?>
