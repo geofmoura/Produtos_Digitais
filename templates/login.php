14aa10eb1be83b75b19b7c7fc10d5db4838977e3
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2025-03-28 01:00:23 -0300

    mudança de extensão

---
+++ templates/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form id="formLogin">
+        <input type="email" name="email" placeholder="E-mail" required>
+        <br><br>
+        <input type="password" name="senha" placeholder="Senha" required>
+        <br><br>
+        <button type="submit">Entrar</button>
+    </form>
+    <p id="mensagemLogin"></p>
+</body>
+
+</html>
+
